8c53db09718635181a0d41d512f08660cfeb3a7c
viseeeee <luca.visentin.1e@iisviolamarchesini.edu.it>
2026-06-20 15:44:09 +0200

    eliminati file debug

--- FrontEnd th/trova_cartella.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo '<pre>';
-echo 'PHP version: ' . PHP_VERSION . "\n";
-echo 'php.ini path: ' . php_ini_loaded_file() . "\n";
-echo 'Extra ini files: ' . php_ini_scanned_files() . "\n";
-echo 'OS: ' . PHP_OS . "\n";
-echo '</pre>';
